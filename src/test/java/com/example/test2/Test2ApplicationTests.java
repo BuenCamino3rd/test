6961c411a76b7994ff0897fb6dc0fc390e8e50d2
ShadowOfShadow <limcw3@gmail.com>
2022-01-12 21:14:42 +0900

    githubaction test

--- src/test/java/com/example/test2/Test2ApplicationTests.java
+++ src/test/java/com/example/test2/Test2ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.test2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Test2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

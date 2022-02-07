49fb955c6a8e2597670bada1c65b5dbf81b90e98
ShadowOfShadow <limcw3@gmail.com>
2022-02-07 20:04:31 +0900

    merge test1

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -8,10 +8,13 @@ public class test {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat formatter = new SimpleDateFormat("EE MMM d y H:m:s ZZZ");
-		String dateString = formatter.format(new Date());
+		SimpleDateFormat nowtime = new SimpleDateFormat("y-MM-dd HH:mm:ss");
+		String dateString = nowtime.format(new Date());
 		System.out.println(dateString);
 		
+		String dueDate = nowtime.format(new Date(System.currentTimeMillis() + 3600000));
+		System.out.println(dueDate); // 만료시간(실제로 보낼 것)
+		
+		
 	}
-
 }
